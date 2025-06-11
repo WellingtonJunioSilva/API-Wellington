d44e353f6eaa994d650480bfe93b2415d27d8659
Caio Messi <136396933+caiomccunha@users.noreply.github.com>
2025-06-11 15:23:20 -0300

    Add files via upload

---
+++ Trabalho TCC V de Vigilância/src/main/java/TCC/Trabalho/TCC/V/de/Vigilancia/Model/TipoApoiador.java
@@ -0,0 +1,10 @@
+package TCC.Trabalho.TCC.V.de.Vigilancia.Model;
+
+public enum TipoApoiador {
+    PESSOA_FISICA ("Pessoa Física"),
+    ONG ("ONG"),
+    EMPRESAS_COMERCIO ("Empresas/Comércio"),
+    PARCEIRO_CONVENIADO ("Parceiro Conveniado");
+
+    
+}
