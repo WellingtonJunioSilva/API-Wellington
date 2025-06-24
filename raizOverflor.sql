934ac88c25d43af726fe1d480e1666fc38ad76e8
Caio Messi <caiomessi19@gmail.com>
2025-06-23 21:44:37 -0300

    Criação apenas das telas, sem conexão com API de cadastro de apoiadores e produtores

--- raizOverflor.sql
+++ raizOverflor.sql
@@ -5,23 +5,27 @@ use raiz_overfror;
 create table produtor(
 id bigint primary key auto_increment not null,
 nome varchar (200) not null,
+email VARCHAR(200) not null,
 documento char (11) not null,
-localizacao varchar (500) not null,
-biografia text not null,
+cep varchar (500) not null,
 cidade varchar (60) not null,
 estado varchar (60) not null,
-fotoPerfil TEXT
+contato VARCHAR (300) not null,
+fotoPerfil TEXT,
+biografia text not null
 );
 
 create table apoiador (
 id bigint primary key unique auto_increment not null,
 nome varchar (200) not null,
+email VARCHAR(200) not NULL,
 tipo enum ('PESSOA_FISICA','ONG', 'EMPRESAS/COMERCIO', 'CONVENIADO') not null,
+cep VARCHAR (200) not null,
 cidade varchar (60) not null,
 estado varchar (60) not null,
 contato varchar (100) not null,
-foto varchar (3000),
-documento char (11)
+documento char (11),
+foto TEXT
 );
 
 create table demandaProdutor(
