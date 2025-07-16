313b6fe6426d445e56abc1bccd04021d1aed972b
caiomccunha <caiomessi19@gmail.com>
2025-07-15 21:24:45 -0300

    fiz algumas alterações no banco de dados

--- raizOverflor.sql
+++ raizOverflor.sql
@@ -1,8 +1,9 @@
-create database raiz_overfror;
+-- Active: 1752542636482@@127.0.0.1@3306@mysql
+CREATE database raiz_overfror;
 
 use raiz_overfror;
 
-create table Usuario(
+create table usuario(
 id bigint primary key auto_increment not null,
 nome varchar (200) not null,
 email VARCHAR(200) not null UNIQUE,
@@ -15,11 +16,20 @@ tipo_usuario enum ('APOIADOR', 'PRODUTOR') not null,
 tipo_apoiador enum ('PESSOA_FISICA', 'ONG', 'EMPRESA/COMERCIO, CONVENIADO'),
 biografia text,
 foto_perfil text
-
 );
 
+alter table usuario 
+add column senha varchar (50);
 
-create table demandaProdutor(
+SET SQL_SAFE_UPDATES = 1;
+
+UPDATE usuario SET senha = 'temporaria' WHERE senha IS NULL;
+
+alter table usuario
+modify column senha varchar (50) not null;
+
+
+create table demandas(
 id bigint auto_increment primary key unique not null,
 idUsuario bigint,
 titulo varchar (250) not null,
@@ -29,22 +39,7 @@ dataPostagem datetime default current_timestamp,
 status enum ('aberta', 'fechada'),
 cidade VARCHAR (60) NOT NULL,
 estado VARCHAR (60) not NULL,
-foreign key (idUsuario) references Usuario (id)
-);
-
-create table demandaApoiador(
-id bigint primary key auto_increment not null unique,
-idUsuario bigint,
-titulo varchar (250) not null,
-descricao text not null,
-cidade VARCHAR (60) NOT NULL, 
-estado VARCHAR (60) NOT NULL,
-dataPostagem datetime default current_timestamp,
 validade_oferta date,
-status enum ('aberta', 'fechada'),
-
-foreign key (idUsuario) references Usuario (id),
+foreign key (idUsuario) references usuario (id)
 );
-
-
 
