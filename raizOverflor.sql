162e849711d0b4e41ec568e2c9234fe79dcb2d4f
Caio Messi <caiomessi19@gmail.com>
2025-07-30 19:24:12 -0300

    Atualizei o banco de dados

--- raizOverflor.sql
+++ raizOverflor.sql
@@ -1,8 +1,8 @@
-CREATE database raiz_overfror;
-
+-- Active: 1750714113071@@127.0.0.1@3306@raiz_overfror
+create database raiz_overfror;
 use raiz_overfror;
 
-create table Usuario(
+create table usuario(
 id bigint primary key auto_increment not null,
 nome varchar (200) not null,
 email VARCHAR(200) not null UNIQUE,
@@ -14,23 +14,16 @@ telefone VARCHAR (300) not null,
 tipo_usuario enum ('APOIADOR', 'PRODUTOR') not null,
 tipo_apoiador enum ('PESSOA_FISICA', 'ONG', 'EMPRESA/COMERCIO, CONVENIADO'),
 biografia text,
-foto_perfil text
+foto_perfil longblob,
+senha varchar (50) not null
 );
 
-alter table Usuario 
-add column senha varchar (50);
-
-SET SQL_SAFE_UPDATES = 1;
-
-UPDATE Usuario SET senha = 'temporaria' WHERE senha IS NULL;
-
-alter table Usuario
-modify column senha varchar (50) not null;
-
+alter table usuario 
+add column foto_perfil mediumblob;
 
 create table demandas(
 id bigint auto_increment primary key unique not null,
-idUsuario bigint,
+id_usuario bigint not NULL,
 titulo varchar (250) not null,
 descricao text not null,
 categoria enum ('graos', 'feijoes_raizes', 'frutas_hortalicas', 'verduras_ervas', 'outros'),
@@ -39,8 +32,28 @@ status enum ('aberta', 'fechada'),
 cidade VARCHAR (60) NOT NULL,
 estado VARCHAR (60) not NULL,
 validade_oferta date,
-foreign key (idUsuario) references Usuario (id)
+tipo_apoio enum ('compra_direta', 'investimento_financeiro', 'infraestrutura', 'maquinario_equipamentos'),
+foreign key (id_usuario) references usuario (id)
+ON DELETE CASCADE
+);
+CREATE TABLE post (
+	id BIGINT NOT NULL AUTO_INCREMENT,
+	message TEXT,
+	likes INT DEFAULT 0,
+	nome_arquivo_post VARCHAR(255),
+	tipo_mime_post VARCHAR(50),
+	foto_post LONGBLOB,
+	PRIMARY KEY (id)
 );
 
-
+CREATE TABLE comment (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    content TEXT,
+    post_id BIGINT,
+    PRIMARY KEY (id),
+    CONSTRAINT fk_comment_post
+        FOREIGN KEY (post_id)
+        REFERENCES post(id)
+        ON DELETE CASCADE
+);
 select * from usuario;
